a65264b8d3900be4e287b83b457f2b10e8e96bbf
neote250 <nornano@student.neumont.edu>
2025-10-17 15:56:24 -0600

    buttons set up, lacking actual function

---
+++ Assets/UI_Folder/UIManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnShopButtonPress()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void OnSettingsButtonPress()
+    {
+        
+    }
+    public void OnTalkButtonPress()
+    {
+        
+    }
+    public void OnLivingRoomButtonPress()
+    {
+        
+    }
+    public void OnBathroomButtonPress()
+    {
+        
+    }
+    public void OnYardButtonPress()
+    {
+        
+    }
+    public void OnToyButtonPress()
+    {
+        
+    }
+    public void OnFoodButtonPress()
+    {
+        
+    }
+    public void OnSoapButtonPress()
+    {
+        
+    }
+
+}
